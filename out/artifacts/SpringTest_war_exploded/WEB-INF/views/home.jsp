31dafd7bb10dc8cd316934fa65753279a9158264
Koseohyeon <ksh030818@naver.com>
2024-03-19 20:34:24 +0900

    톰켓연결연습

---
+++ out/artifacts/SpringTest_war_exploded/WEB-INF/views/home.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 고서현
+  Date: 2024-03-19
+  Time: 오후 5:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Info</title>
+</head>
+<body>
+<h1>홍길동</h1>
+<h3>학번 :${num} </h3>
+</body>
+</html>
---
+++ src/main/java/inhatc/cse/spring/controller/HomeController.java
@@ -0,0 +1,17 @@
+package inhatc.cse.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping( "/")
+    public String home(Model model){
+        model.addAttribute("num","1234567");
+        return "home";
+    }
+
+}
